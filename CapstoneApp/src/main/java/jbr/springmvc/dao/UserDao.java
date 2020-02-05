3db02c65c1f0eee6a7541e83be0b55bc9de4ae4e
BALAMBIKA <BALAMBIKA@DESKTOP-5KQTIIQ>
2020-02-05 11:52:59 +0530

    Initial Commit.
    Pushing all the files.

---
+++ CapstoneApp/src/main/java/jbr/springmvc/dao/UserDao.java
@@ -0,0 +1,15 @@
+/**************************************************************************
+ * 
+ * Author : Balambika Ganesan
+ * 
+ **************************************************************************/
+
+package jbr.springmvc.dao;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public interface UserDao {
+  void register(User user);
+  User validateUser(Login login);
+}
